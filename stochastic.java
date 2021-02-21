6c41a96b312a8fedf29bd14143c113a5e420fc31
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-02-21 14:02:44 -0600

    Create stochastic.java

---
+++ stochastic.java
@@ -0,0 +1,17 @@
+import java.lang.Math; //math library
+
+public class stochastic
+{
+  public static void main(String[] args)
+  {
+    /* no need to modify this section */
+    double a = Double.parseDouble(args[0]);
+    double b = Double.parseDouble(args[1]);
+
+    double randDecimal = Math.random();
+
+    /* TODO: write a function to print a random number in range [a,b)
+    using an equation involving randDecimal, a, and b  */
+
+  }
+}
