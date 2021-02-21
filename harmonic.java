73c501938bf7c6ad304a6e1eb26d14278e84d705
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-02-21 14:02:32 -0600

    Create harmonic.java

---
+++ harmonic.java
@@ -0,0 +1,13 @@
+public class harmonic
+{
+  public static void main(String[] args)
+  {
+    /* no need to modify this section */
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]); 
+
+    /* calculate the harmonic mean of a and b here 
+    and use System.out.println() to print it out */
+
+  }
+}
